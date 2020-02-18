1fbdb0c67786e6ad66ec8839145bafad844989c5
Gene24609 <manhbv24609@gmail.com>
2020-02-18 14:31:28 +0700

    Login module v1

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, EmailField, validators
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.local import LocalProxy
 from functools import wraps
@@ -37,17 +37,69 @@ def create_app(test_config = None):
     
     #Index
     @app.route('/')
-        def index():
-            return render_template('home.html')
+    def index():
+        return render_template('home.html')
 
     #About
     @app.route('/about')
-        def about():
-            return render_template('about.html')
+    def about():
+        return render_template('about.html')
 
     #Login
-    from . import auth
-    app.register_blueprint(auth.bp)
+    #from . import auth
+    #app.register_blueprint(auth.bp)
+
+    #Register Form Class
+    class RegisterForm(Form):
+        name = StringField('Name',[
+            validators.Length(min=1, max= 50),
+            validators.DataRequired("Hãy nhập tên của bạn!")
+        ])
+
+        username = StringField('Username',[
+            validators.Length(min=4,max=25),
+            validators.DataRequired("Hãy nhập Username!")
+        ])
+
+        email = EmailField('Email',[
+            validators.Length(min=6,max=50),
+            validators.Required("Hãy nhập email của bạn!"),
+            validators.Email()
+        ])
+
+        password = PasswordField('Password', [
+            validators.DataRequired(),
+            validators.EqualTo('confirm', message='Passwords do not match'),
+            validators.Email()
+        ])
+        confirm = PasswordField('Confirm Password')
+
+    #User Register
+    @app.route('/register', method=['GET','POST'])
+    def register():
+        form = RegisterForm(request.form)
+        if request.method == 'POST' and form.validate():
+            name = form.name.data
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+        create cursor
+        cur = LocalProxy(get_db)    
+        error = None
+
+        if not username:
+            error = 'Username là trường bắt buộc'
+        elif not email:
+            error = 'Email là trường bắt buộc'
+        elif not name:
+            error = 'Tên là trường bắt buộc'
+        elif not password:
+            error = 'Mật khẩu là trường bắt buộc'
+        elif "SELECT username, email FROM users WHERE username = ?",[username]).fetchone() is not None:
+            error = 'Tên tài khoản {} đã tồn tại. Hãy chọn tên khác!'.format(username)
+        elif "SELECT username, email FROM users WHERE email = ?",[email]).fetchone() is not None:
+            error = 'Email này {} đã được sử dụng. Hãy nhập email khác!'.format(email)
 
 
 
